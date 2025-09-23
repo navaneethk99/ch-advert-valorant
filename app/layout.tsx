a795c576c2f1f49e2eed8022f6d5ee968dfa30a4
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-09-23 20:30:05 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Free Valorant Skins!",
@@ -12,6 +13,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  <Analytics />;
   return (
     <html lang="en">
       <head>
